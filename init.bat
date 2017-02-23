5997523a6a34b7ef31840d7255df029b196631e5
Eric D. Schabell <eric@schabell.org>
2017-02-23 08:54:22 +0100

    Added download info for products.

--- init.bat
+++ init.bat
@@ -81,7 +81,7 @@ if exist %SRC_DIR%\%EAP% (
         echo Product EAP sources are present...
         echo.
 ) else (
-        echo Need to download %EAP% package from http://developers.redhat.com
+        echo Need to download %EAP% package from https://developers.redhat.com/products/eap/download
         echo and place it in the %SRC_DIR% directory to proceed...
         echo.
         GOTO :EOF
@@ -91,7 +91,7 @@ if exist %SRC_DIR%\%BPMS% (
         echo Product BPM Suite sources are present...
         echo.
 ) else (
-        echo Need to download %BPMS% package from http://developers.redhat.com
+        echo Need to download %BPMS% package from https://developers.redhat.com/products/bpmsuite/download
         echo and place it in the %SRC_DIR% directory to proceed...
         echo.
         GOTO :EOF
